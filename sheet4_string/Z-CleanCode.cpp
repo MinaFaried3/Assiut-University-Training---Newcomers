1f3de36cbebc28b047e88e9bcf2372f9f5b039e2
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-06-20 20:46:49 +0200

    finish string

--- sheet4_string/Z-CleanCode.cpp
+++ sheet4_string/Z-CleanCode.cpp
@@ -5,4 +5,41 @@
 using namespace std;
 int main()
 {
+    string source;
+    bool isStartMultiComment = 0;
+    while (getline(cin, source))
+    {
+        bool isNotComment = false;
+        if (source.size() == 0 || source == " ")
+        {
+            continue;
+        }
+
+        for (int i = 0; i < source.size(); i++)
+        {
+            if (source[i] == '/' && source[i + 1] == '/' && !isStartMultiComment)
+            {
+                break;
+            }
+            else if (source[i] == '/' && source[i + 1] == '*')
+            {
+                i++;
+                isStartMultiComment = true;
+            }
+            else if (source[i] == '*' && source[i + 1] == '/' && isStartMultiComment)
+            {
+                i++;
+                isStartMultiComment = false;
+            }
+            else if (!isStartMultiComment)
+            {
+
+                cout << source[i];
+                isNotComment = true;
+            }
+        }
+
+        if (isNotComment && !isStartMultiComment)
+            cout << endl;
+    }
 }
