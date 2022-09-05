59bc9b75ef6ad1f0cc0a8424cb48f01761e85482
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-09-05 15:44:14 +0200

    add templetes

---
+++ sheet9_general_medium/R-KseniaandPanScales.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <cstring>
+#include <vector>
+using namespace std;
+#define lol long long int
+int main()
+{
+}
