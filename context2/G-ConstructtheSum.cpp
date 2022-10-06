3338ed1e23a70d79043b1cd1808ab8799cb8b174
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-10-06 20:50:58 +0200

    solve all problems

--- context2/G-ConstructtheSum.cpp
+++ context2/G-ConstructtheSum.cpp
@@ -1,23 +1,93 @@
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
 using namespace std;
+#define lol long long int
+#define endl '\n'
+void fast()
+{
+    cin.tie(0);
+    cout.sync_with_stdio(0);
+}
+
 int main()
 {
-    long long times, n, s, sum = 0;
+    fast();
+    long long times, num, sum, result = 0;
     cin >> times;
-    for (int i = 1; i <= times; i++)
+
+    vector<lol> vec;
+    while (times--)
     {
-        cin >> n >> s;
-        for (int j = 1; j <= n; j++)
+        /*
+        5 3
+        2 1
+
+
+        6 4
+        3 1
+
+        7 10
+        4 3 2 1
+
+        2 10
+        -1
+
+        */
+        cin >> num >> sum;
+        if (num >= sum)
         {
-            sum += j;
-            cout << j << " ";
-            if (sum == s)
-            {
-                break;
-            }
+            if (((sum * (sum + 1) / 2) < num))
+                cout << -1;
+            else
+                for (lol i = sum - 1; i >= 1; i--)
+                {
+                    if (result + i <= sum)
+                    {
+                        vec.push_back(i);
+                        result += i;
+                    }
+
+                    if (result == sum)
+                        break;
+                }
+
+            if (result == sum)
+                for (lol i = 0; i < vec.size(); i++)
+                    cout << vec[i] << " ";
+        }
+        else
+        {
+            if (((num * (num + 1) / 2) < sum))
+                cout << -1;
+            else
+                for (lol i = num - 1; i >= 1; i--)
+                {
+                    if (result + i <= sum)
+                    {
+                        vec.push_back(i);
+                        result += i;
+                    }
+
+                    if (result == sum)
+                        break;
+                }
+
+            if (result == sum)
+                for (lol i = 0; i < vec.size(); i++)
+                    cout << vec[i] << " ";
         }
 
-        sum = 0;
+        vec.clear();
+        result = 0;
         cout << endl;
     }
 }
--- context2/H-SimpleMod.cpp
+++ context2/H-SimpleMod.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <tuple>
+#include <cmath>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
 using namespace std;
+#define lol long long int
+#define endl '\n'
+void fast()
+{
+    cin.tie(0);
+    cout.sync_with_stdio(0);
+}
+
 int main()
 {
-    int num, x, y;
+    fast();
+    int num;
     cin >> num;
-    
+
+    cout << "0 0";
 }
