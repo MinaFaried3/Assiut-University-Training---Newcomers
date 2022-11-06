3d9e43e95a94de6d5d6b40dcc1a5c4dc171b1292
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-11-06 20:12:29 +0200

    add sol2 to the last two digit

--- a-sheet1_data_type/Y-Thelast2digits.cpp
+++ a-sheet1_data_type/Y-Thelast2digits.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-int main()
+
+void solution1()
+{
+    long long a, b;
+    long long c, d;
+    cin >> a >> b >> c >> d;
+    long long equation = a % 100 * b % 100 * c % 100 * d % 100;
+    if (equation <= 9)
+    {
+        cout << "0" << equation;
+    }
+    else
+    {
+        cout << equation;
+    }
+}
+
+void solution2()
 {
     long long a, b;
     long long c, d;
@@ -18,4 +35,8 @@ int main()
     {
         cout << x % 100;
     }
+}
+int main()
+{
+    solution1();
 }
